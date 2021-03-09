c1071e51470a46edf5c922acdbe7de8c481fc321
mrbean26 <adam@tracycobbold.plus.com>
2021-03-09 19:13:46 +0000

    notes on structure, also see desc
    -changed output training data to either values of  0 or 1, representing if vocals are there or not

--- 2 - Main Project/Audio Seperator and Notation Viewer Main Project/main.cpp
+++ 2 - Main Project/Audio Seperator and Notation Viewer Main Project/main.cpp
@@ -36,7 +36,7 @@ vector<vector<float>> generateInputs(int samplesPerChunk, int samplesPerOverlap,
 	return result;
 }
 
-vector<vector<float>> generateOutputs(int samplesPerChunk, int samplesPerOverlap, int frequencyResolution, int chunksPerInputHalf) {
+vector<vector<float>> generateOutputs(int samplesPerChunk, int samplesPerOverlap, int frequencyResolution, int chunksPerInputHalf, float outputThreshold) {
 	vector<vector<float>> result;
 
 	for (int f = 1; f < 3; f++) {
@@ -49,8 +49,11 @@ vector<vector<float>> generateOutputs(int samplesPerChunk, int samplesPerOverlap
 				float value = fullAudioInput[i][f];
 
 				// Remove NaN values, very very rare bug in visual studio
-				if (isnan(value)) {
+				if (isnan(value) || value < outputThreshold) {
 					value = 0.0f;
+				}
+				if (value >= outputThreshold) {
+					value = 1.0f;
 				}
 
 				currentInput.push_back(value);
@@ -63,19 +66,21 @@ vector<vector<float>> generateOutputs(int samplesPerChunk, int samplesPerOverlap
 }
 
 int main() {
-	int samplesPerChunk = 8192;
+	int samplesPerChunk = 8192; // I think this should be a power of 2
 	int samplesPerOverlap = samplesPerChunk; // no overlap
 
-	int frequencyResolution = 64;
-	int chunkBorder = 20;
+	int frequencyResolution = 64; // Each float represents (sampleRate / frequencyResolution) frequencies
+	int chunkBorder = 20; // How many chunks are added to each side of the input chunk, giving audio "context"
+
+	float outputThreshold = 0.1f; // If values in output vector are above this, they will be set to 1, if not then 0
 
 	vector<vector<float>> inputSet = generateInputs(samplesPerChunk, samplesPerOverlap, frequencyResolution, chunkBorder);
-	vector<vector<float>> outputSet = generateOutputs(samplesPerChunk, samplesPerOverlap, frequencyResolution, chunkBorder);
+	vector<vector<float>> outputSet = generateOutputs(samplesPerChunk, samplesPerOverlap, frequencyResolution, chunkBorder, outputThreshold);
 	
 	int inputSize = inputSet[0].size();
 	int outputSize = outputSet[0].size();
 
-	vector<int> layers = { inputSize, inputSize, outputSize, outputSize, outputSize };
+	vector<int> layers = { inputSize, inputSize, inputSize, outputSize, outputSize };
 	vector<int> biases = { 1, 1, 1, 1, 1, 1, 1  };
 
 	NeuralNetwork network = NeuralNetwork(layers, biases, "tanh");
