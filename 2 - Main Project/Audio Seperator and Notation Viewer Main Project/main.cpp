a2bbc05470f9c559173183031742dc9878bfdc22
mrbean26 <adam@tracycobbold.plus.com>
2021-08-23 12:28:08 +0100

    added big train algorithm to main.cpp

--- 2 - Main Project/Audio Seperator and Notation Viewer Main Project/main.cpp
+++ 2 - Main Project/Audio Seperator and Notation Viewer Main Project/main.cpp
@@ -33,23 +33,114 @@ int main() {
 	int inputSize = inputSet[0].size();
 	int outputSize = outputSet[0].size();
 
-	vector<int> layers = { inputSize, 128, 128, 128, 128, 128, outputSize };
-	vector<int> biases = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-	vector<int> activations = { SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID };
+	// Big Train Algorithm
+	int minLayers = 4;
+	int maxLayers = 12;
+
+	int minSize = 1;
+	int maxSize = 4;
+
+	int biasMin = 0;
+	int biasMax = 3;
+
+	NeuralNetwork::standardTrainConfig forStochastic = NeuralNetwork::standardTrainConfig();
+	forStochastic.epochs = 1000;
+	forStochastic.trainInputs = inputSet;
+	forStochastic.trainOutputs = outputSet;
+	forStochastic.momentum = 0.25f;
+	forStochastic.learningRate = 1.0f;
+	forStochastic.learningRateType = CYCLICAL_LEARNING_RATE;
+	forStochastic.trainType = STOCHASTIC_GRADIENT_DESCENT;
+	forStochastic.batchSize = 250;
+	forStochastic.useStochasticDataset = true;
+	forStochastic.stochasticDatasetSize = 250;
+
+	for (int l = minLayers; l < maxLayers; l++) {
+		for (int n = minSize; n < maxSize; n++) {
+			for (int b = biasMin; b < biasMax; b++) {
+				vector<int> layers = { inputSize };
+				vector<int> biases = { b };
+				vector<int> activations = { TANH };
+
+				for (int i = 0; i < l; i++) {
+					layers.push_back(n * 100);
+					biases.push_back(b);
+					activations.push_back(TANH);
+				}
+
+				layers.push_back(outputSize);
+				biases.push_back(b);
+				activations.push_back(TANH);
+
+				// Gradient Descent
+				NeuralNetwork newStochasticNetwork = NeuralNetwork(layers, biases, activations);
+				vector<float> stochasticErrors = newStochasticNetwork.train(forStochastic);
+
+				
+
+				// Natural Selection
+				NeuralNetwork selectionNetwork = NeuralNetwork::trainNaturalSelectionMethod(forStochastic, layers, biases, activations);
+
+				// Find lowest error of best selection network here
+				int datasetCount = inputSet.size();
+				int outputCount = outputSet[0].size();
+
+				float selectionError = 0.0f;
+				for (int d = 0; d < datasetCount; d++) {
+					vector<float> prediction = selectionNetwork.predict(inputSet[d]);
+
+					for (int o = 0; o < outputCount; o++) {
+						selectionError += abs(prediction[o] - outputSet[d][o]);
+					}
+				}
+				outputImageConfig imageConfig = outputImageConfig{
+					stochasticErrors,
+
+					1000,
+					512,
+
+					newStochasticNetwork,
+					audioConfig,
+					forStochastic,
+
+					true,
+					1000.0f,
+					0.0f,
+				};
+
+				imageConfig.errors = stochasticErrors;
+				imageConfig.network = newStochasticNetwork;
+				imageConfig.trainConfig = forStochastic;
+
+				if (selectionError < stochasticErrors[999]) {
+					imageConfig.trainConfig.trainType = LEVENBERG_MARQUARDT;
+					imageConfig.errors[999] = selectionError;
+				}
+								
+				writeToImage(imageConfig);
+			}
+		}
+	}
+	
 		 
 	// Some Version of Backpropagation Here 	
+	vector<int> layers = { inputSize, 128, 128, 128, 128, 128, 128, outputSize };
+	vector<int> biases = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	vector<int> activations = { SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID, SIGMOID };
+
 	NeuralNetwork newNetwork = NeuralNetwork(layers, biases, activations);
 	
 	NeuralNetwork::standardTrainConfig newConfig = NeuralNetwork::standardTrainConfig();
 	newConfig.trainInputs = inputSet;
 	newConfig.trainOutputs = outputSet;
 
-	newConfig.epochs = 25000;
-	newConfig.learningRate = 0.5f;
-	newConfig.momentum = 0.0f;
+	newConfig.epochs = 1000;
+	newConfig.learningRate = 0.4f;
+	newConfig.momentum = 0.05f;
 	newConfig.learningRateType = FIXED_LEARNING_RATE;
 
-	newConfig.trainType = BATCH_GRADIENT_DESCENT;
+	newConfig.trainType = STOCHASTIC_GRADIENT_DESCENT;
+	newConfig.batchSize = 250;
 
 	vector<float> errors = newNetwork.train(newConfig);
 	createOutputTestTrack(newNetwork, audioConfig);
