67bc606bfdc2aa77caa4fc76d6e3def5141476d2
mrbean26 <adam@tracycobbold.plus.com>
2022-02-05 16:34:57 +0000

    openal implementation into notation project, multiple wave function in openal side project

--- 3 - C++ Notation/OpenAL Test Implementation/OpenAL Test Implementation/main.cpp
+++ 3 - C++ Notation/OpenAL Test Implementation/OpenAL Test Implementation/main.cpp
@@ -1,24 +1,92 @@
 #include <al.h>
 #include <alc.h>
-#include <math.h>
-#include <stdio.h>
-#include <limits.h>
+
+#include <iostream>
+#include <vector>
+using namespace std;
 
 // Example page - https://ffainelli.github.io/openal-example/
 // Page i found this code - https://github.com/hideyuki/openal-sine-wave
 
-#define SECOND 2.0f
 #define SAMPLING_HZ 44100
-#define BUFFER_LENGTH (SECOND * SAMPLING_HZ)
-#define SOUND_HZ 1000.0f
 #define M_PI 3.14159265359
+
+void closeOpenAL(ALuint source, ALuint buffer, ALCcontext* context, ALCdevice* device) {
+    alSourceStop(source);
+    alDeleteSources(1, &source);
+    alDeleteBuffers(1, &buffer);
+    alcMakeContextCurrent(NULL);
+    alcDestroyContext(context);
+    alcCloseDevice(device);
+}
+
+vector<ALshort> generateSinWave(float frequency, float volume, float length, int sampleRate) {
+    vector<ALshort> resultantWave;
+
+    int sampleCount = length * sampleRate;
+    for (int i = 0; i < sampleCount; i++) {
+        float multiplier = float(i) / float(sampleRate);
+
+        ALshort sinValue = sin(2 * M_PI * frequency * multiplier) * SHRT_MAX * volume;
+        ALshort antiphaseValue = -1 * sin(2 * M_PI * frequency * multiplier) * SHRT_MAX * volume;
+
+        resultantWave.push_back(sinValue);
+        resultantWave.push_back(antiphaseValue);
+    }
+
+    return resultantWave;
+}
+vector<ALshort> accumulativeSinWave(vector<float> frequencies, vector<float> volumes, vector<float> lengths, vector<float> offsets) {
+    vector<vector<ALshort>> waves;
+
+    // Get waves
+    int waveCount = frequencies.size();
+    for (int i = 0; i < waveCount; i++) {
+        vector<ALshort> offsetWave = generateSinWave(0.0f, 0.0f, offsets[i], SAMPLING_HZ);
+        vector<ALshort> currentWave = generateSinWave(frequencies[i], volumes[i], lengths[i], SAMPLING_HZ);
+
+        offsetWave.insert(offsetWave.end(), currentWave.begin(), currentWave.end());
+        waves.push_back(offsetWave);
+    }
+
+    // Average waves
+    vector<ALshort> resultantWave;
+    
+    for (int i = 0; i < waveCount; i++) {
+        int sampleCount = waves[i].size();
+        int currentWaveSize = resultantWave.size();
+
+        if (sampleCount > currentWaveSize) {
+            resultantWave.resize(sampleCount, 0);
+        }
+        
+        // Average wave
+        for (int j = 0; j < sampleCount; j++) {
+            if (waves[i][j] == 0) {
+                continue;
+            }
+
+            int activeWaveCount = 0;
+            for (int k = 0; k < waveCount; k++) {
+                if (waves[k][j] != 0) {
+                    activeWaveCount = activeWaveCount + 1;
+                }
+            }
+
+            // Sum
+            resultantWave[j] = resultantWave[j] + waves[i][j] / activeWaveCount;
+        }
+
+
+    }
+
+    return resultantWave;
+}
 
 int main() {
     ALCdevice* device;
     ALCcontext* context;
-    ALshort data[int(BUFFER_LENGTH) * 2];
     ALuint buffer, source;
-    int i;
 
     // Initialization
     device = alcOpenDevice(NULL);
@@ -26,30 +94,24 @@ int main() {
     alcMakeContextCurrent(context);
     alGenBuffers(1, &buffer);
 
-    // Generate sine wave data
-    for (i = 0; i < BUFFER_LENGTH; ++i) {
-        data[i * 2] = sin(2 * M_PI * SOUND_HZ * (float(i) / float(SAMPLING_HZ))) * SHRT_MAX;
-        data[i * 2 + 1] = -1 * sin(2 * M_PI * SOUND_HZ * (float(i) / float(SAMPLING_HZ))) * SHRT_MAX; // antiphase
-    }
+    // Generate wave
+    vector<float> frequencies = { 200.0f, 800.0f };
+    vector<float> volumes = { 1.0f, 0.5f };
+    vector<float> lengths = { 2.0f, 1.0f };
+    vector<float> offsets = { 0.0f, 1.0f };
+
+    vector<ALshort> genData = accumulativeSinWave(frequencies, volumes, lengths, offsets);
 
     // Output looping sine wave
-    alBufferData(buffer, AL_FORMAT_STEREO16, data, sizeof(data), SAMPLING_HZ);
+    alBufferData(buffer, AL_FORMAT_STEREO16, &genData[0], genData.size() * sizeof(ALshort), SAMPLING_HZ);
     alGenSources(1, &source);
     alSourcei(source, AL_BUFFER, buffer);
     alSourcei(source, AL_LOOPING, AL_FALSE);
     alSourcePlay(source);
 
     // Wait to exit
-    printf("Press any key to exit.");
-    getchar();
-
-    // Finalization
-    alSourceStop(source);
-    alDeleteSources(1, &source);
-    alDeleteBuffers(1, &buffer);
-    alcMakeContextCurrent(NULL);
-    alcDestroyContext(context);
-    alcCloseDevice(device);
+    system("pause");
+    closeOpenAL(source, buffer, context, device);
 
     return 0;
 }
