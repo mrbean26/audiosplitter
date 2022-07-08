0b5aa56dce61c5921e3e3d961d9a06b02c9192fb
mrbean26 <adam@tracycobbold.plus.com>
2022-07-08 14:11:52 +0100

    split some rhcp songs. Finished!!

--- 2 - Main Project Splitter/Audio Seperator and Notation Viewer Main Project/main.cpp
+++ 2 - Main Project Splitter/Audio Seperator and Notation Viewer Main Project/main.cpp
@@ -7,7 +7,13 @@ int main() {
 	srand(time(NULL));
 	
 	splitter newSplitter = splitter(STEM_VOCAL);
-	newSplitter.splitStems(STEMS_ALL, "test_tracks/riptide.mp3", "test_vocal_backing/");
+	
+	newSplitter.splitStems(STEMS_VOCALS_BACKING, "rhcpSongs/Californication.mp3", "rhcpSongs/Californication/");
+	newSplitter.splitStems(STEMS_VOCALS_BACKING, "rhcpSongs/Snow.mp3", "rhcpSongs/Snow/");
+	newSplitter.splitStems(STEMS_VOCALS_BACKING, "rhcpSongs/Dani California.mp3", "rhcpSongs/Dani California/");
+	newSplitter.splitStems(STEMS_VOCALS_BACKING, "rhcpSongs/Wet Sand.mp3", "rhcpSongs/Wet Sand/");
+	newSplitter.splitStems(STEMS_VOCALS_BACKING, "rhcpSongs/Under The Bridge.mp3", "rhcpSongs/Under The Bridge/");
+	newSplitter.splitStems(STEMS_VOCALS_BACKING, "rhcpSongs/Scar Tissue.mp3", "rhcpSongs/Scar Tissue/");
 
 	system("pause");
 	return 0;
